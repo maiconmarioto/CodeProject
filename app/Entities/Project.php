1ab509dd4e0f7775ad607aa34bee2f66d047ebc6
Maicon Marioto <maicon.marioto@gmail.com>
2016-02-09 01:11:42 +0000

    Fase 2

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -8,6 +8,8 @@ class Project extends Model
 {
     protected $fillable = [
         'name',
+        'owner_id',
+        'client_id',
         'description',
         'progress',
         'status',
